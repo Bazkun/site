2c14a6ad7078344208fe73707f003dff8fbe5624
Bazkun <krut.pahan@internet.ru>
2025-04-03 21:50:16 +0300

    init

---
+++ main.js
@@ -0,0 +1,127 @@
+/* console.log('Hello, World!');
+let enterName = prompt('Привет, как тебя зовут?');
+document.writeln(`<p>Вас зовут ${enterName}</p>`);
+let enterYear = prompt('В каком году вы родились?');
+let currentYear = 2025;
+let calcAge = currentYear - Number(enterYear);
+document.writeln(`<p>Ваш возраст ${calcAge}</p>`);
+*/
+/* {
+
+
+    let age = 18;
+    console.log(age);
+}
+{
+    let age = 22;
+    console.log(age);
+}
+
+let UserName = "Паша";
+
+{
+    let greet = "Привет, ";
+    console.log(greet, UserName);
+
+    {
+        let question = "Как дела?";
+        console.log(question);
+
+        {
+            let message = "Сегодня хороший день для учёбы!";
+            console.log(message);
+        }
+    }
+} */
+
+/*    let index = 0;
+    let currentYear = 2025;
+    
+    
+    document.writeln("<h1>Список студентов:</h1>");
+    
+    
+    function printStudents(studentName, dateOfBirth, height, faculty) {
+        let age = currentYear - dateOfBirth;
+        index++;
+    
+    
+        document.writeln(`
+            <p>
+                ${index}
+                <strong>
+                    ${studentName}
+                </strong>,
+                Возраст:
+                <strong>
+                    ${age}
+                </strong>,
+                Рост:
+                <strong>
+                    ${height}
+                </strong>,
+                Факультет:
+                <strong>
+                    ${faculty}
+                </strong>
+            </p>
+        `);
+    }
+    
+    
+    printStudents("Вася", 1998, 168, "Исторический");
+    printStudents("Юрий", 2000, 175, "Юридический");
+    printStudents("Мария", 2003, 155, "Биологический");
+    
+    let userName = "Иван";*/
+
+/*    let enterAge = prompt(`Привет, сколько тебе лет?`);
+
+    function isAdult(enterAge) {
+        document.writeln(`<p>Покупай водяру!</p>`)
+        return enterAge < 18
+    }
+    document.writeln(`<p>Вы ещё молодой, вам нельзя водку!</p>`);*/
+/*    function checkScore(score) {
+        return score > 100
+      }
+      const s1 = 115
+      const s2 = 15
+      const s3 = 20
+      console.log(checkScore(s1));*/
+
+/*document.createElement("h1");
+let title = document.createElement("h1");
+title.textContent = "Привет, я заголовок!";
+title.classList.add("title-h1");
+let desc = document.createElement("p");
+desc.textContent = "Привет, я параграф!";
+    let list = document.createElement("ul");
+    let listItem1 = document.createElement("li");
+    listItem1.textContent = "Элемент 1";
+    let listItem2 = document.createElement("li");
+    listItem2.textContent = "Элемент 2";
+    let listItem3 = document.createElement("li");
+    listItem3.textContent = "Элемент 3";
+    list.append(listItem1, listItem2, listItem3);
+let button = document.createElement("button");
+button.textContent = "Добавить";
+button.onclick = function() {
+    let listItem = document.createElement("li");
+    listItem.textContent = "Элемент 4";
+    list.append(listItem);
+    desc.classList.add(`desc-p`);
+}
+document.body.append(title, desc, list, button); */
+
+/*let age = Number(prompt("Введите возраст:"));
+document.writeln(`<p>Возраст пользователя: ${age}</p>`);
+
+if (age >= 18) {
+    // Код который будет выполнен, если условие верное
+    document.writeln("<p>Пользователь совершеннолетний</p>");
+}
+else {
+    document.writeln("<p>Пользователь ебаный молокосос</p>");
+}*/
+
